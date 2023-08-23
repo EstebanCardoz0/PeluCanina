0a5d2ac98e0f0025589940527f8bf6d889e37b26
esteban cardozo <esteban.cardozo.wec@gmail.com>
2023-08-23 12:06:02 -0300

    23 ago

--- src/main/java/peluCanina/peluCanina/controller/MascotaController.java
+++ src/main/java/peluCanina/peluCanina/controller/MascotaController.java
@@ -3,6 +3,7 @@ package peluCanina.peluCanina.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import peluCanina.peluCanina.entity.Duenio;
 import peluCanina.peluCanina.entity.Mascota;
 import peluCanina.peluCanina.service.IMascotaService;
 
@@ -36,6 +37,19 @@ public class MascotaController {
     public void borrarMascota(@PathVariable Long id) {
 
         mascoSer.borrarMascota(id);
+    }
+
+    @PutMapping("/editar/{id}")
+    public Mascota editarMascota(@PathVariable Long id, @RequestParam String nombre,
+            @RequestParam String color, @RequestParam String raza, @RequestParam String atencionEspecial, @RequestParam String alergico,
+            @RequestParam String observaciones, @RequestParam Duenio duen) {
+
+        Mascota mas = new Mascota(id, nombre, color, raza, atencionEspecial, alergico, observaciones, duen);
+
+        mascoSer.editarMascota(mas);
+
+        return mascoSer.traerMascota(id);
+
     }
 
 }//final
